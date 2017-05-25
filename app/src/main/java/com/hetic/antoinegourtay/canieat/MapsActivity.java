d4b95dedc6c600df6960a2d33889d62f3b21cc70
Antoine Gourtay <antoine.gourtay@hetic.net>
2017-05-25 16:27:30 +0200

    Adding functional geolocation feature

--- app/src/main/java/com/hetic/antoinegourtay/canieat/MapsActivity.java
+++ app/src/main/java/com/hetic/antoinegourtay/canieat/MapsActivity.java
@@ -1,16 +1,39 @@
 package com.hetic.antoinegourtay.canieat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.projection.MediaProjectionManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import butterknife.ButterKnife;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 102;
+    private static final String LOCATION_APP = "HappyCow";
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private double latitude;
+    private double longitude;
+    private LatLng currentPosition;
 
 
     @Override
@@ -22,7 +45,82 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        ButterKnife.bind(this);
 
+
+        // Plus d'infos
+        // https://developer.android.com/guide/topics/location/strategies.html
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                mMap.clear();
+                Log.d(LOCATION_APP, "location : " + location);
+
+                //Getting the position from the LocationListener
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+                Log.d(LOCATION_APP, "latitude : " + latitude + " - longitude : " + longitude);
+
+                currentPosition = new LatLng(latitude,longitude);
+
+                MarkerOptions marker = new MarkerOptions();
+                marker.position(currentPosition);
+
+                //Adding the current position on the map
+                mMap.addMarker(marker);
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentPosition, 17));
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                provider.toString();
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+                provider.toString();
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                provider.toString();
+            }
+        };
+
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    && ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                                Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+            }
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
     }
 
 
@@ -38,6 +136,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted!
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+                        return;
+                    }
+
+                    long minTime = 1;
+                    float minDistance = 10;
+
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, minTime, minDistance, locationListener);
+
+                } else {
+
+                    // permission denied!
+                }
+                return;
+            }
+        }
     }
 
 
