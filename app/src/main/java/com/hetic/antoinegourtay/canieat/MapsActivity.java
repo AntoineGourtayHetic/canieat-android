2914ab459d5cba257eb5673ff24326ba4a0d95b5
Antoine Gourtay <antoine.gourtay@hetic.net>
2017-05-25 17:08:48 +0200

    Adding commentaries and autocomplete edittext

--- app/src/main/java/com/hetic/antoinegourtay/canieat/MapsActivity.java
+++ app/src/main/java/com/hetic/antoinegourtay/canieat/MapsActivity.java
@@ -5,7 +5,6 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.media.projection.MediaProjectionManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -17,7 +16,6 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import butterknife.ButterKnife;
@@ -39,7 +37,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_main);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -54,6 +52,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
 
+        //When user location changes
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
@@ -67,6 +66,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 currentPosition = new LatLng(latitude,longitude);
 
+                //Creating a marker for user's position
                 MarkerOptions marker = new MarkerOptions();
                 marker.title("Current position");
                 marker.position(currentPosition);
