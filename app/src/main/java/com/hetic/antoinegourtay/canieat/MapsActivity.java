09269de06853fe00525b1405c176739db22c590d
Antoine Gourtay <antoine.gourtay@hetic.net>
2017-05-25 16:04:25 +0200

    Updating project

--- app/src/main/java/com/hetic/antoinegourtay/canieat/MapsActivity.java
+++ app/src/main/java/com/hetic/antoinegourtay/canieat/MapsActivity.java
@@ -1,42 +1,16 @@
 package com.hetic.antoinegourtay.canieat;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.content.ContextCompat;
-import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
-        com.google.android.gms.location.LocationListener,
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private GoogleApiClient mGoogleApiClient;
-    private LatLng currentPosition;
-    private Location mLastLocation;
-    private Marker currentLocationMarker;
-    private LocationRequest mLocationRequest;
-    private final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 1;
 
 
     @Override
