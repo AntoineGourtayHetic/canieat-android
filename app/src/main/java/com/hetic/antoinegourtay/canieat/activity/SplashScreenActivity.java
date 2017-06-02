753aaaf2c12046e4199026140539d58699bede5b
Antoine Gourtay <antoine.gourtay@hetic.net>
2017-06-02 17:26:58 +0200

    Updating Manifest and SlashScreen Java calss

---
+++ app/src/main/java/com/hetic/antoinegourtay/canieat/activity/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.hetic.antoinegourtay.canieat.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hetic.antoinegourtay.canieat.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final long SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, MapsActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
+
